b08062ff56c8c16128ce3b638c3c869cc8d4320b
Ryan Scharfer <rscharfer@gmail.com>
2021-11-18 18:34:30 -0800

    last part

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -30,12 +30,20 @@ const ShoeCard = ({
     : isNewShoe(releaseDate)
       ? 'new-release'
       : 'default'
-
+  const STYLES = {
+    'new-release' : COLORS.secondary,
+    'on-sale': COLORS.primary
+  }
   return (
     <Link href={`/shoe/${slug}`}>
-      <Wrapper>
+      <Wrapper style={{
+        '--background-color': STYLES[variant],
+        '--custom-text-decoration': variant === 'on-sale' ? 'line-through' : 'none'
+      }}>
         <ImageWrapper>
           <Image alt="" src={imageSrc} />
+          {variant === 'on-sale' && <Banner>Sale</Banner>}
+          {variant === 'new-release' && <Banner>Just Released!</Banner>}
         </ImageWrapper>
         <Spacer size={12} />
         <Row>
@@ -44,6 +52,7 @@ const ShoeCard = ({
         </Row>
         <Row>
           <ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
+          {variant === 'on-sale' && <SalePrice>{formatPrice(salePrice)}</SalePrice>}
         </Row>
       </Wrapper>
     </Link>
@@ -55,6 +64,7 @@ const Link = styled.a`
   color: inherit;
   flex: 1;
   min-width: 340px;
+  
 `;
 
 const Wrapper = styled.article``;
@@ -65,18 +75,26 @@ const ImageWrapper = styled.div`
 
 const Image = styled.img`
   width: 100%;
+  border-radius: 16px 16px 4px 4px;
 `;
 
 const Row = styled.div`
   font-size: 1rem;
+  position: relative;
 `;
 
 const Name = styled.h3`
   font-weight: ${WEIGHTS.medium};
   color: ${COLORS.gray[900]};
+
 `;
 
-const Price = styled.span``;
+const Price = styled.span`
+  position: absolute;
+  top: 0;
+  right: 0;
+  text-decoration: var(--custom-text-decoration);
+`;
 
 const ColorInfo = styled.p`
   color: ${COLORS.gray[700]};
@@ -85,6 +103,21 @@ const ColorInfo = styled.p`
 const SalePrice = styled.span`
   font-weight: ${WEIGHTS.medium};
   color: ${COLORS.primary};
+  position: absolute;
+  top: 0;
+  right: 0;
 `;
 
+
+const Banner = styled.div`
+  position: absolute;
+  top: 12px;
+  right: -4px;
+  background-color: var(--background-color);
+  color: white;
+  font-size: .875rem;
+  padding: 8px 10px;
+  font-weight: 700;
+  border-radius: 2px;
+`
 export default ShoeCard;
